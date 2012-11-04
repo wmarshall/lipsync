d1b2bce848c72667b1a02e722b83e93d66531737
Will Marshall <gridcbug@gmail.com>
2012-11-03 22:24:26 -0400

    removed useless try-catch blocks

--- lipsync.py
+++ lipsync.py
@@ -43,60 +43,44 @@ class LipSyncBase():
     def init_table(self, table):
         cur = self.conn.cursor()
         cols = self.get_cols(table, omit_local = False)
-        try:
-            if UUID_COL_NAME not in cols:
-                cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + UUID_COL_NAME + ' UUID')
-            if LOCAL_ID_COL_NAME not in cols:
-                cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + LOCAL_ID_COL_NAME + ' SERIAL PRIMARY KEY')
-            self.conn.commit()
-        except Exception as e:
-            self.conn.rollback()
-            raise e
+        if UUID_COL_NAME not in cols:
+            cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + UUID_COL_NAME + ' UUID')
+        if LOCAL_ID_COL_NAME not in cols:
+            cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + LOCAL_ID_COL_NAME + ' SERIAL PRIMARY KEY')
+        self.conn.commit()
 
     def update_table(self, table):
         self.init_table(table)
         cur = self.conn.cursor()
-        try:
-            cur.execute('SELECT MIN(' + LOCAL_ID_COL_NAME + '), MAX(' +
-                        LOCAL_ID_COL_NAME + ') FROM ' + table + ' WHERE ' +
-                        UUID_COL_NAME + ' IS NULL')
-            min, max = cur.fetchone();
-            print min
-            print max
-            for i in range(min, max + 1):
-                cur.execute('UPDATE ' + table + ' SET ' + UUID_COL_NAME +
-                ' = %(u)s WHERE ' + LOCAL_ID_COL_NAME + ' = %(i)s',
-                {'u': uuid4().hex, 'i': i})
-            self.conn.commit()
-        except Exception as e:
-            self.conn.rollback()
-            raise e
+        cur.execute('SELECT MIN(' + LOCAL_ID_COL_NAME + '), MAX(' +
+                    LOCAL_ID_COL_NAME + ') FROM ' + table + ' WHERE ' +
+                    UUID_COL_NAME + ' IS NULL')
+        min, max = cur.fetchone();
+        print min
+        print max
+        for i in range(min, max + 1):
+            cur.execute('UPDATE ' + table + ' SET ' + UUID_COL_NAME +
+            ' = %(u)s WHERE ' + LOCAL_ID_COL_NAME + ' = %(i)s',
+            {'u': uuid4().hex, 'i': i})
+        self.conn.commit()
 
     def get_col_map(self, table, omit_local = True):
         cur = self.conn.cursor()
         cols = []
-        try:
-            cur.execute('SELECT * FROM ' + table)
-            cols = [ col.name for col in cur.description ]
-            if omit_local:
-                cols.remove(LOCAL_ID_COL_NAME)
-            self.conn.commit()
-        except Exception as e:
-            self.conn.rollback()
-            raise e
+        cur.execute('SELECT * FROM ' + table)
+        cols = [ col.name for col in cur.description ]
+        if omit_local:
+            cols.remove(LOCAL_ID_COL_NAME)
+        self.conn.commit()
         return cols
 
     def get_uuid_map(self, table):
         cur = self.conn.cursor()
         uuids = []
-        try:
-            cur.execute('SELECT ' + UUID_COL_NAME + ' FROM ' + table)
-            for uuid in cur.fetchall():
-                uuids.append(uuid[0])
-            self.conn.commit()
-        except Exception as e:
-            self.conn.rollback()
-            raise e
+        cur.execute('SELECT ' + UUID_COL_NAME + ' FROM ' + table)
+        for uuid in cur.fetchall():
+            uuids.append(uuid[0])
+        self.conn.commit()
         return uuids
 
     def process_auth(self, fp):
