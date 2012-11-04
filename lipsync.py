496a25454c393817f49f448f55058749e04286b4
Will Marshall <gridcbug@gmail.com>
2012-11-03 20:44:22 -0400

    initial work on data structures

--- lipsync.py
+++ lipsync.py
@@ -6,8 +6,10 @@
 #
 # How it works:
 # Wrapper - JSON
-# HandShake - Symmetric Encryption of a passphrase, both sides validate
-# - Tablename + row order(for hashes) from client
+# HandShake -
+# - 1. Symmetric Encryption of a passphrase, both sides validate : TODO
+# - 2. TableName + row order(for hashes) from client
+# - 3. Lipsync_Continue = True from
 # Status - SHA-1 hashes of each row
 # Request - Each side asks for all of the rows they want
 # Response - Each side responds with row hash + row data
@@ -19,20 +21,21 @@ import json
 UUID_COL_NAME = '_lipsync_uuid'
 LOCAL_ID_COL_NAME = '_local_lipsync_id'
 
-class LSWrapper():
+class LipSync():
     """WARNING: ONLY SUPPORTS POSTGRESQL/Psycopg2 AS OF 11/3"""
     def __init__(self, connection):
         self.conn = connection
 
     def init_table(self, table):
         cur = self.conn.cursor()
-        cols = self.get_maps(table, omit_local = False)
+        cols = self.get_cols(table, omit_local = False)
         try:
             if UUID_COL_NAME not in cols:
-                cur.execute('ALTER TABLE %(t)s ADD COLUMN ' + UUID_COL_NAME + ' UUID PRIMARY KEY', {'t': table})
+                cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + UUID_COL_NAME + ' UUID')
             if LOCAL_ID_COL_NAME not in cols:
-                cur.execute('ALTER TABLE %(t)s ADD COLUMN ' + LOCAL_ID_COL_NAME + ' SERIAL PRIMARY KEY', {'t': table})
-        except e:
+                cur.execute('ALTER TABLE ' + table + ' ADD COLUMN ' + LOCAL_ID_COL_NAME + ' SERIAL PRIMARY KEY')
+            self.conn.commit()
+        except Exception as e:
             self.conn.rollback()
             raise e
 
@@ -40,43 +43,53 @@ class LSWrapper():
         self.init_table(table)
         cur = self.conn.cursor()
         try:
-            cur.execute('SELECT MIN(' + LOCAL_ID_COL_NAME + '), MAX(' + LOCAL_ID_COL_NAME + ') FROM %(t)s WHERE ' + UUID_COL_NAME + ' IS NULL', {'t': table})
+            cur.execute('SELECT MIN(' + LOCAL_ID_COL_NAME + '), MAX(' + LOCAL_ID_COL_NAME + ') FROM ' + table + ' WHERE ' + UUID_COL_NAME + ' IS NULL')
             min, max = cur.fetchone();
-            for i in range(min, max+1):
-                cur.execute('UPDATE %(t)s SET ' + UUID_COL_NAME + ' = %(u)S WHERE ' + LOCAL_ID_COL_NAME + ' = %(i)s', {'t': table, 'u': uuid4(), 'i': i})
+            print min
+            print max
+            for i in range(min, max + 1):
+                cur.execute('UPDATE ' + table + ' SET ' + UUID_COL_NAME + ' = %(u)s WHERE ' + LOCAL_ID_COL_NAME + ' = %(i)s', {'u': uuid4().hex, 'i': i})
             self.conn.commit()
-        except e:
+        except Exception as e:
             self.conn.rollback()
             raise e
 
-    def get_maps(self, table, omit_local = True):
+    def get_col_map(self, table, omit_local = True):
         cur = self.conn.cursor()
-        uuids = []
-        hashes = []
         cols = []
         try:
-            cur.execute('SELECT ' + UUID_COL_NAME + ' FROM %(t)s', {'t': table})
-            for uuid in cur.fetchall():
-                uuids.append(uuid[0])
-            cur.execute('SELECT * FROM %(t)s', {'t': table})
-            for row in cur.fetchall():
-                if omit_local:
-                    row.remove(LOCAL_ID_COL_NAME)
-                hashes.append(sha1(''.join(row).hexdigest())
+            cur.execute('SELECT * FROM ' + table)
             cols = [ col.name for col in cur.description ]
             if omit_local:
                 cols.remove(LOCAL_ID_COL_NAME)
             self.conn.commit()
-        except e:
+        except Exception as e:
             self.conn.rollback()
             raise e
-        uuid_hash_map = zip(uuids, hashes)
-        return (uuid_hash_map, cols)
+        return cols
 
-    def create_handshake_message(self, table):
+    def get_uuid_map(self, table):
+        cur = self.conn.cursor()
+        uuids = []
+        try:
+            cur.execute('SELECT ' + UUID_COL_NAME + ' FROM ' + table)
+            for uuid in cur.fetchall():
+                uuids.append(uuid[0])
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
+        return uuids
+
+    def create_handshake_message_client(self, table):
         return json.dumps({
                     'table' : table,
-                    'cols' : self.get_maps(table)[1]
+                    'cols' : self.get_col_map(table)
+                    })
+
+    def create_handshake_message_server(self, accept=True):
+        return json.dumps({
+                    'Lipsync_Continue':accept
                     })
 
 
