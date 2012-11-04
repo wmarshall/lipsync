e06321d22545e2ae44fb1ca32fb721c03c547f54
Will Marshall <gridcbug@gmail.com>
2012-11-03 22:23:28 -0400

    initial client-server structure

--- lipsync.py
+++ lipsync.py
@@ -6,14 +6,18 @@
 #
 # How it works:
 # Wrapper - JSON
+# Lock - Lock the database/ your view of it and enter a transaction
 # HandShake -
 # - 1. Symmetric Encryption of a passphrase, both sides validate : TODO
+# - - 1. "ENCRYPTEDPASS" - no other objects, just string
+# - - 2. {LipSync_Auth = False} + Disconnect if Auth not acceptable
+# - - 3. Else {LipSync_Auth = True}
 # - 2. TableName + row order(for hashes) from client
-# - 3. Lipsync_Continue = True from
-# Status - SHA-1 hashes of each row
+# Status - UUIDS of each row
 # Request - Each side asks for all of the rows they want
 # Response - Each side responds with row hash + row data
-# Terminate - Lipsync_Exit = True in an otherwise empty object
+# Terminate - LipSync_Exit = True in an otherwise empty object
+# Commit - Commit the transaction
 from hashlib import sha1
 from uuid import uuid4
 import json
@@ -21,7 +25,17 @@ import json
 UUID_COL_NAME = '_lipsync_uuid'
 LOCAL_ID_COL_NAME = '_local_lipsync_id'
 
-class LipSync():
+class LipSyncError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return str(self.message)
+
+class AuthError(LipSyncError):
+    pass
+
+class LipSyncBase():
     """WARNING: ONLY SUPPORTS POSTGRESQL/Psycopg2 AS OF 11/3"""
     def __init__(self, connection):
         self.conn = connection
@@ -43,12 +57,16 @@ class LipSync():
         self.init_table(table)
         cur = self.conn.cursor()
         try:
-            cur.execute('SELECT MIN(' + LOCAL_ID_COL_NAME + '), MAX(' + LOCAL_ID_COL_NAME + ') FROM ' + table + ' WHERE ' + UUID_COL_NAME + ' IS NULL')
+            cur.execute('SELECT MIN(' + LOCAL_ID_COL_NAME + '), MAX(' +
+                        LOCAL_ID_COL_NAME + ') FROM ' + table + ' WHERE ' +
+                        UUID_COL_NAME + ' IS NULL')
             min, max = cur.fetchone();
             print min
             print max
             for i in range(min, max + 1):
-                cur.execute('UPDATE ' + table + ' SET ' + UUID_COL_NAME + ' = %(u)s WHERE ' + LOCAL_ID_COL_NAME + ' = %(i)s', {'u': uuid4().hex, 'i': i})
+                cur.execute('UPDATE ' + table + ' SET ' + UUID_COL_NAME +
+                ' = %(u)s WHERE ' + LOCAL_ID_COL_NAME + ' = %(i)s',
+                {'u': uuid4().hex, 'i': i})
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()
@@ -81,16 +99,43 @@ class LipSync():
             raise e
         return uuids
 
-    def create_handshake_message_client(self, table):
+    def process_auth(self, fp):
+        if not self.auth_valid(json.load(fp)):
+            json.dump({'LipSyncAuth': False})
+            raise AuthError('Invalid passphrase, aborting')
+
+    def send_auth(self, fp):
+        fp.write(self.create_auth_message())
+
+    def auth_valid(self, auth):
+        return json.loads(auth) == 'TODO'
+
+    def create_auth_message(self, key):
+        return json.dumps('TODO')
+
+class LipSyncClient(LipSyncBase):
+    def create_handshake_message(self, table):
         return json.dumps({
                     'table' : table,
                     'cols' : self.get_col_map(table)
                     })
 
-    def create_handshake_message_server(self, accept=True):
+
+class LipSyncServer(LipSyncBase):
+    def create_handshake_message(self, accept=True):
         return json.dumps({
-                    'Lipsync_Continue':accept
+                    'LipSync_Continue': accept
                     })
+
+    def sync(self, fp):
+        try:
+            self.process_auth(fp)
+            self.send_auth(fp)
+        finally:
+            fp.close()
+
+
+
 
 
 
--- lstest.py
+++ lstest.py
@@ -24,18 +24,20 @@
 import sys
 import psycopg2
 import psycopg2.extras
-from lipsync import LipSync
+from lipsync import LipSyncClient, LipSyncServer
 
 
 def main():
     conn = psycopg2.connect(connection_factory=psycopg2.extras.DictConnection,
                             dbname = 'signins')
 
-    ls = LipSync(conn)
-    if len(sys.argv) > 1 and sys.argv[1] = 'server':
-        print ls.create_handshake_message_server()
+    if len(sys.argv) > 1 and sys.argv[1] == 'server':
+        lss = LipSyncServer(conn)
+        print lss.create_auth_message('test')
         return 0
-    print ls.create_handshake_message_client('signin_log')
+
+    lsc = LipSyncClient(conn)
+    print lsc.auth_valid(lsc.create_auth_message('signin_log') )
     return 0
 
 if __name__ == '__main__':
